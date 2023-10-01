cf389291e63fd7565e563a4d84b1962540874e9a
yuriiweblack <yuriiwebblack@gmail.com>
2023-10-01 20:00:13 +0300

    login&logout

--- apps/routes.py
+++ apps/routes.py
@@ -1,6 +1,9 @@
 from apps import app
 from flask import render_template, flash, redirect, url_for
 from apps.forms import LoginForm
+from flask_login import current_user, login_user, logout_user
+
+from apps.models import User
 
 
 @app.route('/')
@@ -22,14 +25,30 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+
     form = LoginForm()
     if form.validate_on_submit():
-        flash(f"Login requested for user{form.username.data}, remember_me{form.remember_me.data}, password{form.password.data}")
-        # flash("All GOOD!")
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash("Invalid username or password!")
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        flash(f"User!!!! {user}!")
         return redirect(url_for('index'))
+
+        # flash(f"User!!!! {user}!")
+        # flash(f"Login requested for user{form.username.data}, remember_me{form.remember_me.data}, password{form.password.data}")
+        # flash("All GOOD!")
+        # return redirect(url_for('index'))
     return render_template('login.html', title="Sign In", form=form)
 
 
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
 
 # @app.route('/log')
 # def log():
--- mainblog.py
+++ mainblog.py
@@ -1,1 +1,8 @@
-from apps import app
+from apps import app, db
+from apps.models import User,Post
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
+
+
